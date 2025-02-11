f9f3be0b7a84519baef9522efc42597c7f8431f4
uday <udaykiran9147@gmail.com>
2025-02-11 09:57:09 +0530

    Add proxy server implementation and HTTP request parsing library

---
+++ proxy_server.c
@@ -0,0 +1,33 @@
+// Lib imports
+#include <stdio.h>
+#include<stdio.h>
+
+
+// Local imports
+#include "proxy_parse.h"
+
+typedef struct cache_element cache_element;
+
+struct cache_element {
+    char* data;
+    int len;
+    time_t lru_time;
+    struct cache_element* next;
+};
+
+
+cache_element* find(char* url);
+
+int add_cache_element(char* data, int size, char* url);
+
+void remove_cache_element();
+
+int PORT = 8080;
+
+#include<stdio.h>
+
+int main()
+{
+
+return 0;
+}
